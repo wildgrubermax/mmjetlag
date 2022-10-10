624e9b003d5bf6e0d9429cf31766c7ff8a8337d9
wildgrubermax <59665467+wildgrubermax@users.noreply.github.com>
2022-10-10 11:16:12 -0400

    the city choosing algorithm using integrals
    requires the world_cities_modified.csv file, does not take distance into account, chooses based on integral of normal distribution for each attendee and peak productivity, conference working hours, and a malleable curve for how quickly prod falls off. input is done through the attendees tuple which contains (gmt, number of attendees) pairs

---
+++ choose.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from scipy.integrate import cumtrapz
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#Returns the "height" of the normal function of the given parameters at the point x relative to the mean
+def normalHeight(x,mean,dev):
+    return (1/(np.sqrt(2*np.pi*(dev**2))))*np.exp(-0.5*(((x-mean)/dev)**2))
+
+#Our parameters for the conference
+openingTime = 8
+closingTime = 17
+step = 0.01
+peakprod = 12
+shape = 4
+
+x = np.arange(openingTime, closingTime, step)
+
+df = pd.read_csv("world_cities_modified.csv")
+
+print(df)
+
+cols = [1,10,12]
+gmtdf =  df[df.columns[cols]]
+
+print(gmtdf)
+
+gmtdf['gmtOffset'] = gmtdf['gmtOffset'].divide(60)
+
+#for value in gmtdf['gmtOffset']:
+#    print(value)
+
+attendees = ((6,100),(5,1),(5,2))
+cities = (("a",5),("b",0),("c",-5))
+
+#print(df)
+
+#print(df.iloc[1])
+
+bestCity = ""
+bestScore = 0
+
+for index, city in gmtdf.iterrows():
+    sumScore = 0
+    for attendee in attendees:
+        sumScore += attendee[1]*np.trapz(normalHeight(x,peakprod-attendee[0]+city['gmtOffset'],shape), x)
+        if sumScore > bestScore:
+            bestScore = sumScore
+            bestPop = city['population']
+            bestCity = city['city']+" at gmt:"+str(city['gmtOffset'])
+        if sumScore == bestScore:
+            if city['population'] > bestPop:
+                bestScore = sumScore
+                bestPop = city['population']
+                bestCity = city['city']+" at gmt:"+str(city['gmtOffset'])
+
+print(bestCity)
+print("Population: "+str(bestPop))
+print("Score: "+str(bestScore))
